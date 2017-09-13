ae89453ca67a2ec84f8efca94e0a2e712a2ba609
marcin walendzik <marcin.walendzik@gmail.com>
2017-09-13 22:44:34 +0200

    working transfer and mapping used

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -1,23 +1,46 @@
 contract HelloWorld {
-    uint public balance;
+
+    address public owner;
+    mapping (address => uint) balances;
+
+
+//    uint public balance;
 
     function HelloWorld(){
-        balance = 1000;
+//        balance = 1000;
+        owner = msg.sender;
+        balances[owner] = 1000;
     }
 
-    event BalanceSet(uint val);
-
-    function setBalance(uint val) {
-        balance = val;
-        BalanceSet(balance);
+    function getBalance(address _user) constant returns (uint _balance){
+        return balances[_user];
     }
 
-    function changeBalance(uint val) {
-        balance += val;
-        BalanceSet(balance);
+    function transfer(address _to, uint _val) returns (bool success){
+
+        if(balances[msg.sender] < _val){
+            return false;
+        }
+        balances[msg.sender] -= _val;
+        balances[_to] += _val;
+        return true;
     }
 
-    function getBalance() constant returns (uint) {
-        return balance;
-    }
+//    event BalanceSet(uint val);
+//
+//    function setBalance(uint val) {
+//        balance = val;
+//        BalanceSet(balance);
+//    }
+//
+//    function changeBalance(uint val) {
+//        balance += val;
+//        BalanceSet(balance);
+//    }
+
+//    function getBalance() constant returns (uint) {
+//        return balance;
+//    }
+
+
 }
