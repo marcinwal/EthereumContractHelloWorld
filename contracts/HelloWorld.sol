f21df47c8dbdeff80cbb86187f093db28033a885
marcinwal <marcin.walendzik@gmail.com>
2017-09-14 09:10:08 +0200

    test on Windows machine with tiny changes

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -6,19 +6,19 @@ contract HelloWorld {
 
 //    uint public balance;
 
-    function HelloWorld(){
+    function HelloWorld() {
 //        balance = 1000;
         owner = msg.sender;
         balances[owner] = 1000;
     }
 
-    function getBalance(address _user) constant returns (uint _balance){
+    function getBalance(address _user) constant returns (uint _balance) {
         return balances[_user];
     }
 
-    function transfer(address _to, uint _val) returns (bool success){
+    function transfer(address _to, uint _val) returns (bool success) {
 
-        if(balances[msg.sender] < _val){
+        if(balances[msg.sender] < _val) {
             return false;
         }
         balances[msg.sender] -= _val;
